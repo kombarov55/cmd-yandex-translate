3d77e19c66d8ea6b039296f182cbadc4ec25de2e
Nikolay <kombarov55@gmail.com>
2018-04-18 03:26:50 +0300

    refactoring

--- src/main/kotlin/com/company/App.kt
+++ src/main/kotlin/com/company/App.kt
@@ -18,9 +18,9 @@ fun main(args: Array<String>) {
     }
 }
 
-private val API = "https://translate.yandex.net"
+private val domain = "https://translate.yandex.net"
 private val key = "trnsl.1.1.20180417T211322Z.6b63d18701d4af12.647556fa4d6898394e1c69a15fc344a528c58de3"
-private val url = "$API/api/v1.5/tr.json/translate?key=$key"
+private val url = "$domain/api/v1.5/tr.json/translate?key=$key"
 
 private fun translate(text: String?): List<String> {
     return khttp.post(
--- src/test/kotlin/com/company/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package com.company
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
